d3a94fd54779c467e66c2f8ec02a4cb3db969a77
RENAN JODAS <renanjodas@gmail.com>
2019-03-27 11:22:56 -0300

    Sistema simples em C# de busca de CEP - Webservice

---
+++ buscarCep/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace buscarCep
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Variável de designer necessária.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Limpar os recursos que estão sendo usados.
+		/// </summary>
+		/// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Código gerado pelo Windows Form Designer
+
+		/// <summary>
+		/// Método necessário para suporte ao Designer - não modifique 
+		/// o conteúdo deste método com o editor de código.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.txtCep = new System.Windows.Forms.TextBox();
+			this.btnBuscaCep = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// txtCep
+			// 
+			this.txtCep.Location = new System.Drawing.Point(86, 71);
+			this.txtCep.Name = "txtCep";
+			this.txtCep.Size = new System.Drawing.Size(100, 20);
+			this.txtCep.TabIndex = 0;
+			// 
+			// btnBuscaCep
+			// 
+			this.btnBuscaCep.Location = new System.Drawing.Point(101, 122);
+			this.btnBuscaCep.Name = "btnBuscaCep";
+			this.btnBuscaCep.Size = new System.Drawing.Size(75, 23);
+			this.btnBuscaCep.TabIndex = 1;
+			this.btnBuscaCep.Text = "Buscar CEP";
+			this.btnBuscaCep.UseVisualStyleBackColor = true;
+			this.btnBuscaCep.Click += new System.EventHandler(this.btnBuscaCep_Click);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(274, 230);
+			this.Controls.Add(this.btnBuscaCep);
+			this.Controls.Add(this.txtCep);
+			this.Name = "Form1";
+			this.Text = "Exemplo WebService CEP";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.TextBox txtCep;
+		private System.Windows.Forms.Button btnBuscaCep;
+	}
+}
+
---
+++ buscarCep/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace buscarCep
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void btnBuscaCep_Click(object sender, EventArgs e)
+		{
+			string enderecoCompleto = "";
+			try
+			{
+
+				var webService = new consultaCep.AtendeClienteClient();
+				var resposta = webService.consultaCEP(txtCep.Text);
+				enderecoCompleto += "Endereço: " + resposta.end;
+				enderecoCompleto += "\nBairro: " + resposta.bairro;
+				enderecoCompleto += "\nEstado: " + resposta.uf;
+				MessageBox.Show(enderecoCompleto);
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show("CEP não localizado");
+			}
+		}
+	}
+}
